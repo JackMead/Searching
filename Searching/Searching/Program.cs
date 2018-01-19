0bb2d07143c63f808888106f0548ed67cbd836d6
JackMead <Jack.Mead@softwire.com>
2018-01-19 16:27:13 +0000

    Binary search and linear search implemented

---
+++ Searching/Searching/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Run();
+        }
+
+        public void Run()
+        {
+            var words = LoadWords();
+            var numWords = words.Length;
+            var random = new Random();
+            var searcher = new Searcher();
+            var timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var wordToFind = words[random.Next(numWords)];
+                searcher.LinearSearch(words, wordToFind);
+            }
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds / 1000);
+
+            timer.Restart();
+            for (int i = 0; i < 10000; i++)
+            {
+                var wordToFind = words[random.Next(numWords)];
+                searcher.BinarySearch(words, wordToFind);
+            }
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds / 1000);
+        }
+
+        public string[] LoadWords()
+        {
+            var path = "Resources/Words.txt";
+            var lines = new List<string>();
+            using (var sr = new StreamReader(path))
+            {
+                var line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            lines.Sort();
+            return lines.ToArray();
+        }
+    }
+}
---
+++ Searching/Searching/Searcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    class Searcher
+    {
+        public int BinarySearch(string[] words, string wordToFind)
+        {
+            var length = words.Length;
+            var split = words.Length / 2;
+            var indexToCheck = length / 2;
+            while (words[indexToCheck] != wordToFind)
+            {
+                var compared = string.Compare(words[indexToCheck], wordToFind);
+                if (split > 1) { split /= 2; }
+                if (compared < 0)
+                {
+                    indexToCheck += split;
+                }
+                else
+                {
+                    indexToCheck -= split;
+                }
+            }
+            return indexToCheck;
+        }
+
+        public int LinearSearch(string[] words, string wordToFind)
+        {
+            var index = 0;
+            while (words.Length > index && words[index] != wordToFind)
+            {
+                index++;
+            }
+            return index;
+        }
+    }
+}
